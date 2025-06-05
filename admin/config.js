8bd110e7475ae33080d541cabced025608eb8b22
Jarett <jarett.rude@gmail.com>
2025-06-05 00:05:49 -0700

    chore: update oauth process

--- admin/config.js
+++ admin/config.js
@@ -1,14 +1,10 @@
-// Config for Decap CMS
 window.CMS_MANUAL_INIT = true;
 
-// Initialize the CMS
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
   CMS.init({
     config: {
-      // Use the config.yml file for configuration
       load_config_file: true,
-      // Make sure to include the base_url from your config.yml
-      local_backend: true
-    }
+      local_backend: true,
+    },
   });
 });
