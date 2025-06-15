4d7d91142fd9719a1f1cfca1cacceaf9367c4160
Jarett <jarett.rude@gmail.com>
2025-06-14 22:17:55 -0700

    Debug: custom loading

--- admin/admin.js
+++ admin/admin.js
@@ -1,7 +1,3 @@
 console.log("CMS custom script loaded");
 
-CMS.registerWidget(
-  "text",
-  window.DECAP_CMS_WIDGETS.text.control,
-  window.DECAP_CMS_WIDGETS.text.preview
-);
+CMS.registerPreviewStyle("/assets/css/main.css");
